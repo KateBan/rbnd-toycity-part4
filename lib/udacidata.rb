557a561f222e41d8bb524fecdd7553bbfdc61013
Katerina <kbaneva@gmail.com>
2016-03-12 16:40:38 +0200

    Pass self.all test

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,25 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  @@file = File.dirname(__FILE__) + "/../data/data.csv"
+  
+  def self.create(*attributes)
+  	data = CSV.read(@@file)
+  	item = self.new(*attributes)
+
+  	if item.is_a? Product
+  		create_item = [item.id, item.brand, item.name, item.price]
+  		if !data.include?(create_item)
+  			CSV.open(@@file, "ab") do |row|
+  				row << data
+  			end
+  		end
+  	end
+  	item
+  end
+
+  def self.all
+  	products =[]
+  	
+  end
 end
