56ff9598229d33d873dac7d751ac3b941adc6c70
Katerina <kbaneva@gmail.com>
2016-03-14 14:14:00 +0200

    Add print_report

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -8,7 +8,42 @@ module Analyzable
 		(total/products.length).round(2)
 	end
   
+  def count_by_brand(products)
+  	inventory = Hash.new
+  	products.each do |product|
+  		if inventory[product.brand]
+  			inventory[product.brand] += 1
+  		else
+  			inventory[product.brand] = 1
+  		end
+  	end
+  	inventory
+  end
+
+  def count_by_name(products)
+  	inventory = Hash.new
+  	products.each do |product|
+  		if inventory[product.name]
+  			inventory[product.name] += 1
+  		else
+  			inventory[product.name] = 1
+  		end 
+  	end
+  	inventory
+  end
+
   def print_report(products)
+  	report = "Average price: $#{average_price(products)}"
+  	report += "\n"
+  	report += "Inventory by brand: "
+  	count_by_brand(products).each do |brand, value|
+  		report += "   #{brand}: #{value}"
+  	end
+  	report += "Inventory by name: "
+  	count_by_name(products).each do |name, value|
+  		report += "   #{name}: #{value}"
+  	end
+  	report
   end
  
 end
